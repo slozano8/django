0272126d4a052d9604f0d284fd1e0980c7954033
Santos <95259821+slozano8@users.noreply.github.com>
2024-02-28 12:53:51 -0500

    Update urls.py

--- Django/web_project/urls.py
+++ Django/web_project/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import settings, views
 
 urlpatterns = [
-    path("", include("hello.urls")),
-    path('admin/', admin.site.urls)
-]
+    path('todos/', include('todos.urls')),
+    path('admin/', admin.site.urls),
+    path('', views.index)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
